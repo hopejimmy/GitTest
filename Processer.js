59652ead1d088609e7346f7d0ee6e63757948409
hopejimmy <hopejimmy@gmail.com>
2016-01-24 16:32:34 +0800

    added the 4th comments

--- Processer.js
+++ Processer.js
@@ -1,10 +1,10 @@
 function Processor (abc) {
-	// body...
+	cosnsole.print("this is the 4th branch");	// body...
 
 	console.print("this is the test proccessor");
 	console.print("change the 2nd time");
 	cosnsole.print("this is the 3rd branch");
-
+	cosnsole.print("this is the 4th branch");
 }
 
 exports.Processor = Processor;
